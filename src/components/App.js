faa647eebe6a82b4dcdb59b5d6d0f0527e46b425
SoftBearFeet <SoftBearFeet@users.noreply.github.com>
2017-08-10 13:52:18 -0800

    Imported '{ connect() }' from 'react-redux'  & created function to remap state into an array

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,32 @@
 import React, { Component } from 'react'
-import { addRecipe } from '../actions'
+import { connect } from 'react-redux'
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      calendar: null
-    }
-  }
-  componentDidMount() {
-    const { store } = this.props
-
-    store.subscribe(() => {
-      this.setState(() => ({
-        calendar: store.getState()
-      }))
-    })
-  }
-
-  submitFood = () => {
-    this.props.store.dispatch(addRecipe({
-      day: 'monday',
-      meal: 'breakfast',
-      recipe: {
-        label: this.input.value
-      }
-    }))
-
-    this.input.value= '';
-  }
   render() {
+    console.log('Props', this.props);
     return (
       <div>
-    <input
-        type='text'
-        ref={(input) => this.input = input}
-        placeholder="Monday's Breakfast"
-    />
-    <button onClick={this.submitFood}>Submit</button>
-
-    <pre>
-        Monday's Breakfast: {this.state.calendar && this.state.calendar.monday.breakfast}
-    </pre>
-</div>
+        Hello World
+      </div>
     )
   }
 }
 
-export default App
+function  mapStateToProps (state) {
+const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+
+
+  return {
+    calendar: dayOrder.map((day) => ({
+      day,
+      meals: Object.keys(state[day]).reduce((meals, meal) => {
+        meals[meal] = state[day][meal] ?
+          state[day][meal] : null
+          return meals
+      }, {}),
+    }))
+  }
+}
+
+export default connect(mapStateToProps)(App)
