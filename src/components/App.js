eea49c1807856c6931e0a04bca0b040a6e57df88
SoftBearFeet <SoftBearFeet@users.noreply.github.com>
2017-08-10 14:24:41 -0800

    Updated 'mapStateToProps' to now also pass in 'food'.

--- src/components/App.js
+++ src/components/App.js
@@ -13,17 +13,20 @@ class App extends Component {
   }
 }
 
-function  mapStateToProps (state) {
-const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+function  mapStateToProps ({ calendar, food }) {
+
+
+
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
 
 
 
   return {
     calendar: dayOrder.map((day) => ({
       day,
-      meals: Object.keys(state[day]).reduce((meals, meal) => {
-        meals[meal] = state[day][meal] ?
-          state[day][meal] : null
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal] ?
+          food[calendar[day][meal]] : null
           return meals
       }, {}),
     }))
